c3e0faeadc79ac4c780f6486904c304708d8ab3a
InnaKovshik <kovshikinna@gmail.com>
2021-08-08 17:03:33 +0300

    fixed

--- src/redux/auth/auth-operations.js
+++ src/redux/auth/auth-operations.js
@@ -3,66 +3,66 @@ import actions from '../auth/auth-actions';
 import { BASE_URL } from '../../index';
 //import generateUniqueId from 'generate-unique-id';
 
-axios.defaults.baseURL = BASE_URL;
+axios.defaults.baseURL = 'https://goit-phonebook-api.herokuapp.com/';
 const setAxiosHeaderToken = token =>
-	(axios.defaults.headers.common.Authorization = `Bearer ${token}`);
+  (axios.defaults.headers.common.Authorization = `Bearer ${token}`);
 const unsetAxiosHeaderToken = () =>
-	(axios.defaults.headers.common.Authorization = ``);
+  (axios.defaults.headers.common.Authorization = ``);
 
 const registration = user => async dispatch => {
-	dispatch(actions.registrationRequest());
+  dispatch(actions.registrationRequest());
 
-	try {
-		const response = await axios.post('/users/signup', user);
-		setAxiosHeaderToken(response.data.token);
-		dispatch(actions.registrationSuccess(response.data));
-	} catch (error) {
-		dispatch(actions.registrationError(error));
-	}
+  try {
+    const response = await axios.post('/users/signup', user);
+    setAxiosHeaderToken(response.data.token);
+    dispatch(actions.registrationSuccess(response.data));
+  } catch (error) {
+    dispatch(actions.registrationError(error));
+  }
 };
 
 const login = user => async dispatch => {
-	dispatch(actions.loginRequest());
+  dispatch(actions.loginRequest());
 
-	try {
-		const response = await axios.post('/users/login', user);
-		setAxiosHeaderToken(response.data.token);
-		dispatch(actions.loginSuccess(response.data));
-	} catch (error) {
-		dispatch(actions.loginError(error));
-	}
+  try {
+    const response = await axios.post('/users/login', user);
+    setAxiosHeaderToken(response.data.token);
+    dispatch(actions.loginSuccess(response.data));
+  } catch (error) {
+    dispatch(actions.loginError(error));
+  }
 };
 
 const logout = () => async dispatch => {
-	dispatch(actions.logoutRequest());
+  dispatch(actions.logoutRequest());
 
-	try {
-		await axios.post('/users/logout');
-		unsetAxiosHeaderToken();
-		dispatch(actions.logoutSuccess());
-	} catch (error) {
-		dispatch(actions.logoutError(error));
-	}
+  try {
+    await axios.post('/users/logout');
+    unsetAxiosHeaderToken();
+    dispatch(actions.logoutSuccess());
+  } catch (error) {
+    dispatch(actions.logoutError(error));
+  }
 };
 
 const getUser = () => async (dispatch, getState) => {
-	const {
-		auth: { token: persistedToken },
-	} = getState();
+  const {
+    auth: { token: persistedToken },
+  } = getState();
 
-	if (!persistedToken) {
-		return;
-	}
-	setAxiosHeaderToken(persistedToken);
+  if (!persistedToken) {
+    return;
+  }
+  setAxiosHeaderToken(persistedToken);
 
-	dispatch(actions.getUserRequest());
+  dispatch(actions.getUserRequest());
 
-	try {
-		const response = await axios.get('/users/current');
-		dispatch(actions.getUserSuccess(response.data));
-	} catch (error) {
-		dispatch(actions.getUserError(error.message));
-	}
+  try {
+    const response = await axios.get('/users/current');
+    dispatch(actions.getUserSuccess(response.data));
+  } catch (error) {
+    dispatch(actions.getUserError(error.message));
+  }
 };
 
 export default { registration, login, logout, getUser };
